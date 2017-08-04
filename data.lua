113e5106cad7b2736d981a32427245f9bbf6d530
Ciaran Ewing-James <cewing03@qub.ac.uk>
2017-08-04 23:55:41 +0100

    improve gui, cleanup

---
+++ data.lua
@@ -0,0 +1,18 @@
+data:extend{
+   {
+      type="sprite",
+      name="text-to-speech-logo-sprite",
+      filename = "__Text-To-Speech__/graphics/ttsIcon.png",
+      priority = "extra-high-no-scale",
+      width = 32,
+      height = 32,
+   },
+	 {
+		  type="sprite",
+      name="text-to-speech-submit-sprite",
+      filename = "__Text-To-Speech__/graphics/submitIcon.png",
+      priority = "extra-high-no-scale",
+      width = 32,
+      height = 32,
+	 }
+}
